66f58b54aa1284d36f109602c41efbf69bac3348
Ilja Siros <ilja.sirosh@gmail.com>
2026-03-27 11:29:51 +0100

    Improve crash finder script

--- scripts/crash_finder.py
+++ scripts/crash_finder.py
@@ -1,252 +1,224 @@
-import os
-import glob
-from dataclasses import dataclass
+import argparse
+import re
+from dataclasses import dataclass, field
+from typing import Set, List, Tuple, Dict
 
-# EPOCH_SIZE = (
-#     2  # includes the iteration check at the end (in this case a single iteration)
-# )
-lines = []
-
-
-# NOTE: example, from an older version
-# with open("/home/jakob/Documents/uni/doc/project/COTS_TESTS/ThreadFuzzer/EVE_SENSOR/run_2025-11-18_15-47-12/fuzzer_logs/info/threadfuzzer_info_2025-11-18_15:47:12.log", "r") as f:
-#    lines = f.readlines()
-
-workdir = os.path.join(os.getcwd(), "../logs/_data/EVE_SENSOR")
-print("workdir:", workdir)
-idx = 25  # should be 23, 25 and 10
-file_names = sorted(glob.glob(os.path.join(workdir, "*")))
-print("file_names:", file_names)
-for i, f in enumerate(file_names):
-    if i == idx:
-        print(i, "->", f)
-    else:
-        print(i, f)
-info_file = glob.glob(os.path.join(file_names[idx], "fuzzer_logs/info/*"))[0]
-print(info_file)
-
-
-# info_file  = "/home/jakob/Documents/uni/doc/project/COTS_TESTS/ThreadFuzzer/EVE_SENSOR/run_2025-11-18_15-47-12/fuzzer_logs/info/threadfuzzer_info_2025-11-18_15:47:12.log"
-
-
-# use of generators to quickly parse big files
-def read_lines_gen(fname):
-    with open(fname, "r") as file:
-        for line in file:
-            # if "Dissector wpan" in line:
-            #     continue
-            # if "Fuzzed packet" in line:
-            #     continue
-            # if "<---" in line:
-            #     continue
-            if (
-                "---> Dissector" in line
-                or "Mutator:" in line
-                or "ITERATION" in line
-                or "REBOOTS:" in line
-            ):
-                yield line
-
-
-# with open(info_file) as f:
-#     lines = f.readlines()
-
-markers = {}
-
-
-def packet_extractor():
-    summary = ""
-    mutators = []
-    packets = []
-    for i, line in enumerate(read_lines_gen(info_file)):
-        # print(line)
-        if "ITERATION" in line:
-            print("current_size:", len(packets))
-            markers[len(packets)] = line
-            print((len(packets), line))
-        elif "REBOOTS:" in line:
-            print("current_size:", len(packets))
-            markers[len(packets)] = line
-            print((len(packets), line))
-        elif "---> Dissector's summary" in line:
-            if summary == "":
-                summary = line.split("summary: ")[1]
-            else:
-                packets.append((summary, mutators))
-                # print("extracted:",(summary,mutators))
-                summary = line.split("summary: ")[1]
-                mutators = []
-        elif "Mutator" in line and summary != "":
-            mutators.append(line)
-    if len(mutators) != 0 or summary != "":
-        # print("orphaned_muts:", mutators, "or")
-        # print("orphaned_summ:", summary)
-        if summary != "":
-            packets.append((summary, mutators))
-    return packets
-
+# --- DATA STRUCTURES ---
 
 @dataclass(frozen=True)
 class Mutation:
     field: str
     new_val: str
 
-
 @dataclass
 class MutatedPacket:
     name: str
     packet_type: str
-    mutations: {Mutation}
+    mutations: Set[Mutation]
+    iteration: int = -1
 
+@dataclass
+class EpochData:
+    epoch_id: int
+    normal_iterations: int = 0
+    real_reboots: int = 0
+    start_iteration: int = -1
+    end_iteration: int = -1
+    packets: List[MutatedPacket] = field(default_factory=list)
 
-def crash_matcher(crash, packet):
-    if crash.packet_type in packet.packet_type and crash.mutations.issubset(
-        packet.mutations
-    ):
-        print("Crash:", crash.name)
-        return True
-    return False
+# --- CRASH DEFINITIONS ---
 
-
-# TODO: also create versions, for the different packet types!
-# like c1 can also be written for Data Response
-c1 = MutatedPacket(
-    "crash 1",
-    "Child ID Response",
-    set([Mutation("thread_nwd.tlv.prefix.length", "New value=255")]),
-)
-
-c1_dr = MutatedPacket(
-    "crash 1, data response",
-    "Data Response",
-    set([Mutation("thread_nwd.tlv.prefix.length", "New value=255")]),
-)
-
-c2 = MutatedPacket(
-    "crash 2",
-    "Child ID Response",
-    set(
-        [
-            Mutation("thread_nwd.tlv.len", "New value=0"),
-            Mutation("thread_nwd.tlv.server.16", "New value=40961"),
-        ]
-    ),
-)
-
-c3 = MutatedPacket(
-    "crash 3",
-    "Child ID Response",
-    set([Mutation("thread_nwd.tlv.len", "New value=255")]),
-)
-
-c3_dr = MutatedPacket(
-    "crash 3, data response",
-    "Data Response",
-    set([Mutation("thread_nwd.tlv.len", "New value=255")]),
-)
-
-# c4 = MutatedPacket(
-#     "crash 4",
-#     "Child Update Response",
-#     set([Mutation("mle.tlv.timeout", "New value=4294967295")]),
-# )
-
-c5 = MutatedPacket(
-    "crash 5",
-    "Advertisement",
-    set([Mutation("mle.tlv.leader_data.router_id", "New value=255")]),
-)
-
-
-c9 = MutatedPacket(
-    "crash 9",
-    "Child ID Response",
-    set(
-        [
-            Mutation("mle.tlv.addr16", "New value=65535"),
-            Mutation("mle.tlv.source_addr", "New value=65535"),
-        ]
-    ),
-)
-
-# TODO read in the crashes from JSON, more flexible
-crash_muts = [
-    c1,
-    c1_dr,
-    # c2, # impossible on an MTD
-    c3,
-    c3_dr,
-    # c5, # impossible on an MTD
-    c9,   # extremely rare
+CRASH_SIGNATURES = [
+    MutatedPacket("Crash 1", "Child ID Response", {Mutation("thread_nwd.tlv.prefix.length", "New value=255")}),
+    MutatedPacket("Crash 1", "Data Response", {Mutation("thread_nwd.tlv.prefix.length", "New value=255")}), 
+    MutatedPacket("Crash 3", "Child ID Response", {Mutation("thread_nwd.tlv.len", "New value=255")}),
+    MutatedPacket("Crash 3", "Data Response", {Mutation("thread_nwd.tlv.len", "New value=255")}),
+    MutatedPacket("Crash 4", "Child ID Response", {
+        Mutation("mle.tlv.addr16", "New value=65535"),
+        Mutation("mle.tlv.source_addr", "New value=65535")
+    }),
+    MutatedPacket("Crash 5", "Advertisement", {
+        Mutation("mle.tlv.leader_data.router_id", "New value=255")
+    }),
 ]
 
+# --- PARSING LOGIC ---
 
-cnt = 0
-cnt_max = 1000
-mult = cnt_max // 100
-# print("[", end='')
-# gen = packet_extractor()
-# packet = next(gen)
-# while packet is not None:
-#     packets.append(packet)
-#     packet = next(gen)
-#     # print(packet)
-#     cnt += 1
-#     if (cnt % mult) == 0:
-#         print(cnt)
-#     if cnt > cnt_max:
-#         break
+def parse_log_file(filepath: str) -> List[EpochData]:
+    epochs: List[EpochData] = []
+    current_epoch = EpochData(epoch_id=1)
+    
+    current_summary = ""
+    current_mutations = set()
+    current_iteration = -1 
 
-packets = packet_extractor()
-# print("]")
+    mutation_pattern = re.compile(r"Fuzzed field\s+([a-zA-Z0-9_.]+).*?(New value=[^;]+)")
+    iteration_pattern = re.compile(r"START OF A NEW FUZZING ITERATION\s+(\d+)")
 
-print("collected:", len(packets), "packets")
-# print("leaving:", len(line_buffer), "lines unparsed...")
-print("")
-print("...start analysis...")
-print("")
-cid_cnt = 0
-for i, p in enumerate(packets):
-    # turn into MutatedPacket element
-    raw_muts = p[1]
-    muts = []
-    for rm in raw_muts:
-        try:
-            fname = rm.split("Mutator:")[1].split("Fuzzed field ")[1].split(" ")[0]
-            nval = rm.split("Mutator:")[1].split("Fuzzed field ")[1].split(";")[1][1:]
-            muts.append(Mutation(fname, nval))
-        except IndexError:
-            continue
-        # if 'Child ID Response' in p[0] and 'thread_nwd.tlv.prefix.length' in rm:
-        #     if 'New value=255' in rm:
-        #         print("WE SHOULD FIND THIS:", i, p, muts[len(muts)-1], sep='\n')
-    if len(muts) != 0:
-        # NOTE to check
-        # NOTE end check
-        # if i == 4888:
-        #     print("----------------")
-        #     print(c1.packet_type)
-        #     print(p[0])
-        #     print(c1.mutations)
-        #     print(set(muts))
-        if "Child ID Response" in p[0]:
-            cid_cnt += 1
-        mut_packet = MutatedPacket("input", p[0], set(muts))
-        # print(mut_packet)
-        for crash in crash_muts:
-            # crash_matcher(crash, mut_packet)
-            if crash_matcher(crash, mut_packet):
-                print("at:", i)
-        # crash_matcher(c9, MutatedPacket("input2", p[0], set(muts)))
-        # if c1.packet_type in p[0] and c1.mutations.issubset(set(muts)):
-        #     print("Crash_1:", i)
-        # if c9.packet_type in p[0] and c9.mutations.issubset(set(muts)):
-        #     print("Crash_9:", i)
+    with open(filepath, "r") as file:
+        for line in file:
+            if "START OF A NEW FUZZING ITERATION" in line:
+                current_epoch.normal_iterations += 1
+                match = iteration_pattern.search(line)
+                if match:
+                    current_iteration = int(match.group(1))
+                    
+                    if current_epoch.start_iteration == -1:
+                        current_epoch.start_iteration = current_iteration
+                    current_epoch.end_iteration = current_iteration
+                    
+            elif "REBOOTS:" in line:
+                try:
+                    current_epoch.real_reboots = int(line.split("REBOOTS:")[1].strip())
+                    epochs.append(current_epoch)
+                    current_epoch = EpochData(epoch_id=len(epochs) + 1)
+                except (IndexError, ValueError):
+                    pass
+                
+            elif "---> Dissector's summary" in line:
+                if current_summary:
+                    current_epoch.packets.append(MutatedPacket(
+                        name="parsed_input", 
+                        packet_type=current_summary, 
+                        mutations=current_mutations,
+                        iteration=current_iteration
+                    ))
+                
+                try:
+                    current_summary = line.split("summary: ")[1].strip()
+                except IndexError:
+                    current_summary = "Unknown"
+                current_mutations = set()
 
-    # print possible markers:
-    if i in markers.keys():
-        print("")
-        print(i, markers[i])
+            elif "Mutator" in line and current_summary:
+                match = mutation_pattern.search(line)
+                if match:
+                    field_name, new_val = match.groups()
+                    current_mutations.add(Mutation(field_name, new_val))
+
+        if current_summary:
+            current_epoch.packets.append(MutatedPacket(
+                name="parsed_input", 
+                packet_type=current_summary, 
+                mutations=current_mutations,
+                iteration=current_iteration
+            ))
+            
+        if current_epoch.normal_iterations > 0 or current_epoch.packets:
+            epochs.append(current_epoch)
+
+    return epochs
 
 
-print("cidcnt:", cid_cnt)
+# --- ANALYSIS LOGIC ---
+
+def analyze_crashes(epochs: List[EpochData], active_signatures: List[MutatedPacket], device_type: str):
+    first_occurrence: Dict[str, Tuple[int, int]] = {}
+    
+    # Track using the filtered signatures list
+    crash_counts: Dict[str, int] = {f"{crash.name} [{crash.packet_type}]": 0 for crash in active_signatures}
+
+    total_unexpected_reboots = 0
+    unexpected_reboots_per_epoch = {}
+    
+    epoch_size = max((e.normal_iterations for e in epochs), default=0)
+
+    print(f"\n--- FUZZING ANALYSIS REPORT ({device_type} TARGET) ---")
+    print(f"Detected Fixed Epoch Size: {epoch_size} iterations")
+    
+    # Quick debug print so the user knows exactly what is being tracked
+    tracked_names = sorted(list(set([c.name for c in active_signatures])))
+    print(f"Monitoring for: {', '.join(tracked_names)}")
+
+    global_packet_index = 0 
+    
+    for epoch in epochs:
+        epoch_crash_count = 0
+        
+        for packet in epoch.packets:
+            global_packet_index += 1
+            for crash in active_signatures:
+                if crash.packet_type in packet.packet_type and crash.mutations.issubset(packet.mutations):
+                    
+                    crash_key = f"{crash.name} [{crash.packet_type}]"
+                    crash_counts[crash_key] += 1
+                    epoch_crash_count += 1
+                    
+                    if crash_key not in first_occurrence:
+                        first_occurrence[crash_key] = (global_packet_index, packet.iteration)
+
+        expected_epoch_reboots = 1 + epoch.normal_iterations + epoch_crash_count
+        unexpected_diff = epoch.real_reboots - expected_epoch_reboots
+        
+        if unexpected_diff > 0:
+            iter_range = f"Iterations {epoch.start_iteration}-{epoch.end_iteration}" if epoch.start_iteration != -1 else "No iterations"
+            
+            unexpected_reboots_per_epoch[epoch.epoch_id] = {
+                "range": iter_range,
+                "unexpected": unexpected_diff,
+                "real": epoch.real_reboots,
+                "expected": expected_epoch_reboots,
+                "iterations": epoch.normal_iterations,
+                "crashes": epoch_crash_count
+            }
+            total_unexpected_reboots += unexpected_diff
+
+    # 1. First occurrence of each crash
+    print("\n1. FIRST OCCURRENCE OF EACH CRASH CATEGORY:")
+    if not first_occurrence:
+        print("   No known crashes detected.")
+    else:
+        sorted_firsts = sorted(first_occurrence.items(), key=lambda item: item[0])
+        for crash_key, (idx, iteration) in sorted_firsts:
+            print(f"   - {crash_key}: Packet #{idx} (Iteration {iteration})")
+
+    # 2. Total number of crashes per category
+    print("\n2. TOTAL CRASHES PER CATEGORY:")
+    sorted_counts = sorted(crash_counts.items(), key=lambda item: item[0])
+    for crash_key, count in sorted_counts:
+        print(f"   - {crash_key}: {count}")
+
+    # 3. Unexpected Reboots Per Epoch
+    print("\n3. UNEXPECTED REBOOTS PER EPOCH:")
+    if not unexpected_reboots_per_epoch:
+        print("   None! All reboot counts matched expected values.")
+    else:
+        for epoch_id, stats in unexpected_reboots_per_epoch.items():
+            print(f"   - Epoch {epoch_id} ({stats['range']}): {stats['unexpected']} unexpected reboots "
+                  f"(Real: {stats['real']}, Expected: {stats['expected']} "
+                  f"[1 base + {stats['iterations']} iterations + {stats['crashes']} crashes])")
+        
+        print(f"\n   => TOTAL UNEXPECTED REBOOTS: {total_unexpected_reboots}")
+    print("\n-------------------------------\n")
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Extract fuzzing crash data per epoch from log files.")
+    parser.add_argument("logfile", help="Path to the fuzzer log file to analyze.")
+    # Add the required device flag
+    parser.add_argument("--device", "-d", choices=["MTD", "FTD"], required=True, 
+                        help="Target device type (MTD or FTD). Dictates which crashes are expected.")
+    args = parser.parse_args()
+
+    # Define the mapping of device types to their expected crash names
+    device_expected_crashes = {
+        "MTD": ["Crash 1", "Crash 3", "Crash 4"],
+        "FTD": ["Crash 1", "Crash 3", "Crash 5"]
+    }
+
+    # Filter the global signatures down to only what this device type supports
+    expected_names = device_expected_crashes[args.device]
+    active_signatures = [crash for crash in CRASH_SIGNATURES if crash.name in expected_names]
+
+    print(f"Parsing log file: {args.logfile} for {args.device} device...")
+    
+    epochs = parse_log_file(args.logfile)
+    total_packets = sum(len(e.packets) for e in epochs)
+    
+    print(f"Successfully extracted {total_packets} mutated packets across {len(epochs)} epochs.")
+    
+    # Pass the filtered signatures into the analyzer
+    analyze_crashes(epochs, active_signatures, args.device)
+
+if __name__ == "__main__":
+    main()
